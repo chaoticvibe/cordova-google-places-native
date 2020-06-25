83c9a0a5de0108c78b35958decf1bb006b42ab90
makveli <pedrohenriqueissa@gmail.com>
2020-06-25 14:46:12 -0300

    Add files via upload

--- www/PlaceAutocompleteDialog.js
+++ www/PlaceAutocompleteDialog.js
@@ -1,27 +1,12 @@
-var exec = require("cordova/exec");
-var PLUGIN_NAME = "GooglePlaces";
 
-module.exports = {
-    getPredictions: function(query, options) {
-        return new Promise(function(resolve, reject) {
-            if (options.types == "geocode") {
-                options.types = 1;
-            } else if (options.types == "address") {
-                options.types = 2;
-            } else if (options.types == "establishment") {
-                options.types = 3;
-            } else if (options.types == "(regions)") {
-                options.types = 4;
-            } else if (options.types == "(cities)") {
-                options.types = 5;
-            }
+var exec = require('cordova/exec');
 
-            exec(resolve, reject, PLUGIN_NAME, "getPredictions", [query, options]);
-        });
-    },
-    getById: function(placeId) {
-        return new Promise(function(resolve, reject) {
-            exec(resolve, reject, PLUGIN_NAME, "getById", [placeId]);
-        });
-    }
+var PlaceAutocompleteDialog = {
+
+  show: function(onSuccess, onError) {
+    exec(onSuccess, onError, 'PlaceAutocompleteDialog', 'show', []);
+  }
+
 };
+
+module.exports = PlaceAutocompleteDialog;
