631677cc9a32da8840b039a92d5810a9bef3bbf6
makveli <pedrohenriqueissa@gmail.com>
2020-07-04 19:58:29 -0300

    Update PlacesPlugin.java

--- src/android/cordova/plugins/gms/places/PlacesPlugin.java
+++ src/android/cordova/plugins/gms/places/PlacesPlugin.java
@@ -15,7 +15,8 @@ import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
 import com.google.android.libraries.places.api.model.AutocompletePrediction;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.api.net.FetchPlaceRequest;
-import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.model.AddressComponent;
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
 
 import org.apache.cordova.CallbackContext;
